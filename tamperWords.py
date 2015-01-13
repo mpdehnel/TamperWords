a8553b2cc28676b7873aa0f473a9e366f8f765d0
Katriel Cohn-Gordon <katriel.cohn-gordon@cybersecurity.ox.ac.uk>
2015-01-13 12:36:33 +0000

    pythonise

--- tamperWords.py
+++ tamperWords.py
@@ -1,25 +1,27 @@
+#!/usr/bin/env python
+
 ''' TamperWords, Martin Dehnel Jan 2015. GPLv2.
   Inspired by talks from RWC 2015.
   Basic Idea: Users don't (and should have to) understand what a hash is, 
- 	or how to generate one of some downloaded software.
+     or how to generate one of some downloaded software.
   Replace hashes with 6-10 randomly chosen (but always the same per file) dictionary words
- 	with a similar order of magnitude of entropy, and ask the user to check they're
- 	the same as the ones listed on the website. Words generated from a SHA-256/512 Hash (transparent to user).
+     with a similar order of magnitude of entropy, and ask the user to check they're
+     the same as the ones listed on the website. Words generated from a SHA-256/512 Hash (transparent to user).
 
   Display the TamperWords on screen automatically after software (.exe, .msi, .dmg, .app etc.) is downloaded,
-  	do NOT require the user to do anything.
+      do NOT require the user to do anything.
   Initial setup: Chrome & Firefox Extensions.
   Aim: Full integration with Chrome, Firefox, & other browsers.
 
   Tasks:
-  	x Write core engine (word generation from input at the command-line)
-  	- Write a Chrome/Fx extension to display the words automatically.
-  	- Allow websites to specify a meta-field such as: 
-  		<meta name="download-hash" content="d30d93fbf523ae769208b89c2eb10e12e7572df8898af4e602b2cfc657bcdaba">
-  		<meta name="tamperWords" content="primate subsignation bordar coalitioner ignominiously ecurie">
-  	- Then get this to compare the content from the meta-field to the hash of the download automatically.
+      x Write core engine (word generation from input at the command-line)
+      - Write a Chrome/Fx extension to display the words automatically.
+      - Allow websites to specify a meta-field such as: 
+          <meta name="download-hash" content="d30d93fbf523ae769208b89c2eb10e12e7572df8898af4e602b2cfc657bcdaba">
+          <meta name="tamperWords" content="primate subsignation bordar coalitioner ignominiously ecurie">
+      - Then get this to compare the content from the meta-field to the hash of the download automatically.
 
-  	(sha256("I swear by my pretty floral bonnet, I will end you.") == d30d9...)
+      (sha256("I swear by my pretty floral bonnet, I will end you.") == d30d9...)
 
   Needs a better name. Suggestions welcome!
 
@@ -29,49 +31,37 @@
 
  '''
 
+import argparse
 import hashlib
-import sys
+from functools import partial as p
 
-def tamperWords(filename):
+def tamperWords(filename, dictionary):
+    # Read the file in block by block so as not to destroy memory usage
+    blocksize, hasher = 2**16, hashlib.sha256()
+    with open(filename, 'rb') as myFile:
+        for chunk in iter(p(myFile.read, blocksize), b''):
+            hasher.update(chunk)
 
-	''' Read the file in block by block so as not to destroy memory usage '''
-	block = 65536
-	m = hashlib.sha256()
-	with open(filename, 'rb') as myFile:
-	    myBuffer = myFile.read(block)
-	    while len(myBuffer) > 0:
-			m.update(myBuffer)
-			myBuffer = myFile.read(block)
-	myFile.close()	
+    # hasher = hashlib.sha512('I swear by my pretty floral bonnet, I will end you.')
+    digest = hasher.hexdigest()
 
-	# m = hashlib.sha512('I swear by my pretty floral bonnet, I will end you.')
-	digest = m.hexdigest()
+    # Split the digest into 8 pieces, 128 bits per piece, and mod out to 64 bits each
+    chunks = (digest[(i)*8:(i+1)*8] for i in xrange(8))
+    chunks = (int(chunk, 16) % 2**16 for chunk in chunks)
+    
+    # Load dictionary word list
+    with open(dictionary) as f:
+        words = f.read().split()
 
-	''' Split the digest into 8 pieces, 128 bits per piece '''
-	splitDigest = []
-	for i in xrange(8):
-		splitDigest.append(digest[(i)*8:(i+1)*8])
-
-	''' Reduce to 64 bits per piece by modulo 64k '''
-	moduloDigest = []
-	for item in splitDigest:
-		moduloDigest.append(int(item, 16) % 65536)
-
-	''' Load dictionary word list '''
-	filename = "words1.txt"
-	with open(filename) as f:
-	    words = f.read().split()
-	f.close()
-
-	''' Convert 64-bit digest pieces to words. Choosing 0-65535.'''
-	wordsList = []
-	for chunk in moduloDigest:
-		wordsList.append(words[chunk])
-
-	return " ".join(wordsList)
-
+    # Convert 64-bit digest pieces to words. Choosing 0-65535.
+    return " ".join(words[chunk] for chunk in chunks)
 
 ''' Take filename as input from the command-line '''
-filename = sys.argv[-1]
-print tamperWords(filename)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("filename", help="path to file to be digested")
+    parser.add_argument("--dictionary", help="optional dictionary to use for string generation", default="words1.txt")
+    args = parser.parse_args()
+
+    print tamperWords(args.filename, args.dictionary)
 
--- wordStrip.py
+++ wordStrip.py
@@ -1,22 +1,21 @@
+#!/usr/bin/env python
+
 import sys
+import argparse
 
 ''' Cleans up the 2.5mb word-file and makes it 665kb '''
-
-filename = "words.txt" 
-words = set(x.strip().lower() for x in open(filename))
-# print len(words): 235886
-
-''' Strip wordsList of words shorter than 3 and longer than 9. 
-	Word list is 85983 in length at this point '''
-newWords = []
-for word in words:
-	if len(word) > 3 and len(word) < 9:
-		newWords.append(word)
-
-newWords = sorted(newWords)
-# print newWords
-
-f = open('words1.txt', 'w')
-
-f.write("\n".join(newWords))
-f.close()
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("filename", help="words file to filter", default="words.txt")
+    args = parser.parse_args()
+    
+    with open(args.filename) as f:
+        words = set(x.strip().lower() for x in f)
+    # print len(words): 235886
+    
+    # Strip wordsList of words shorter than 3 and longer than 9. 
+    # Word list is 85983 in length at this point
+    newWords = sorted(word for word in words if len(word) > 3 and len(word) < 9)
+    
+    with open('words1.txt', 'w') as f:
+        f.write("\n".join(newWords))
